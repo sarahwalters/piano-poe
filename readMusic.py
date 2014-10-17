0ba2b2199306c478bed3c46332e60079123dd0c1
Sarah Walters <sarah.walters@students.olin.edu>
2014-10-17 11:28:07 -0400

    Another test commit

--- readMusic.py
+++ readMusic.py
@@ -1,2 +1,3 @@
 """This will become the code to take in a MIDI file and output motor signals
-Right now it's mainly just to test pushing"""
+Right now it's mainly just to test pushing.
+Here's another pushing test!"""
