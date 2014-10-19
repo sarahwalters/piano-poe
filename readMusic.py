d743a9c7fcbb63db6ee79808bd3dd922eceef14c
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-10-18 20:06:13 -0400

    Added a blank music page and code to display it

--- readMusic.py
+++ readMusic.py
@@ -1,2 +1,7 @@
 """This will become the code to take in a MIDI file and output motor signals
 Right now it's mainly just to test pushing"""
+import cv2
+test = cv2.imread('treblestaffsheetblank.jpg')
+print(type(test))
+cv2.imshow('test', test)
+cv2.waitKey(0)
