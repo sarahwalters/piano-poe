528fa15359e5cb6fe0a234fceb77a6c0a53fc816
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-10-18 20:44:02 -0400

    readMusic: Edge detection test on blank music sheet

--- readMusic.py
+++ readMusic.py
@@ -2,8 +2,11 @@
 <<<<<<< HEAD
 Right now it's mainly just to test pushing"""
 import cv2
-test = cv2.imread('treblestaffsheetblank.jpg')
-print(type(test))
-cv2.imshow('test', test)
+test = cv2.imread('treblestaffsheetblank.jpg', 0)
+test1 = cv2.Canny(test,100,200)
+print type(test1)
+
+
+cv2.imshow('test', test1)
 cv2.waitKey(0)
 
