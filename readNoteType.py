fd1cca797ce0e82ea0c78d53f8efcfa0692edfc8
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-10-22 22:18:46 -0400

    readNoteType: implemented SIFT. Although looked promising, not viable strategy
    Finds a lot of keypoints on the individual note, can't accurately match them to a training image with many notes

--- readNoteType.py
+++ readNoteType.py
@@ -3,48 +3,6 @@ import numpy as np
 
 
 
-t_im = cv2.imread('images/Scale1c.png')
-q_im = cv2.imread('images/quarter_note_run.png')
-
-method = 'SIFT'
-#finding keypoints and descriptors in the training image
-detector = cv2.FeatureDetector_create(method)
-descriptor = cv2.DescriptorExtractor_create(method)     
-t_k = detector.detect(t_im)
-t_k, t_d = descriptor.compute(t_im, t_k)
-
-#find all keypoints and descriptors in the query image
-q_k = detector.detect(q_im)
-q_k, q_d = descriptor.compute(q_im, q_k)
-
-
-#saving keypoint coordinates in a list because keypoint objects can't be pickled
-kps =[]
-for point in t_k:
-    kps.append((point.pt[0], point.pt[1]))
-    cv2.circle(t_im, (int(point.pt[0]), int(point.pt[1])), 2, (0, 0, 255), 3)
-
-matcher = cv2.BFMatcher() #(normType = cv2.NORM_HAMMING)
-
-#match list of object keypoints to query image, not other way around
-matches = matcher.knnMatch(t_d, q_d, k = 2)
-
-#Nearest neighbor test to reduce false matches
-good_matches = []
-for m,n in matches:
-    # if m.distance < 0.75*n.distance:
-    # Get coordinate of the match
-    m_x = int(q_k[m.trainIdx].pt[0])
-    m_y = int(q_k[m.trainIdx].pt[1])
-    good_matches.append((m_x, m_y))
-    cv2.circle(q_im, (int(m_x), int(m_y)), 2, (0, 0, 255), 3)
-
-cv2.imshow('show', t_im)
-cv2.waitKey(0)
-
-
-
-
 def mean_shift(hypothesis, keypoints, threshold):
     """
     Inputs:
@@ -96,4 +54,52 @@ def mean_shift(hypothesis, keypoints, threshold):
 
             #difference between the current and last guess
             diff = np.sqrt((last_guess[0] - x)**2 + (last_guess[1] - y)**2)
-            return hypothesis, radius, current
+            return hypothesis
+
+
+
+t_im = cv2.imread('images/note4.png')
+q_im = cv2.imread('images/singleOctaveTemplate.png')
+
+method = 'SIFT'
+#finding keypoints and descriptors in the training image
+detector = cv2.FeatureDetector_create(method)
+descriptor = cv2.DescriptorExtractor_create(method)     
+t_k = detector.detect(t_im)
+t_k, t_d = descriptor.compute(t_im, t_k)
+
+#find all keypoints and descriptors in the query image
+q_k = detector.detect(q_im)
+q_k, q_d = descriptor.compute(q_im, q_k)
+
+
+#saving keypoint coordinates in a list because keypoint objects can't be pickled
+kps =[]
+for point in t_k:
+    kps.append((point.pt[0], point.pt[1]))
+    cv2.circle(t_im, (int(point.pt[0]), int(point.pt[1])), 2, (0, 0, 255), 3)
+
+matcher = cv2.BFMatcher() #(normType = cv2.NORM_HAMMING)
+
+#match list of object keypoints to query image, not other way around
+matches = matcher.knnMatch(t_d, q_d, k = 2)
+
+#Nearest neighbor test to reduce false matches
+good_matches = []
+for m,n in matches:
+    # if m.distance < 0.75*n.distance:
+    # Get coordinate of the match
+    m_x = int(q_k[m.trainIdx].pt[0])
+    m_y = int(q_k[m.trainIdx].pt[1])
+    good_matches.append((m_x, m_y))
+    cv2.circle(q_im, (int(m_x), int(m_y)), 2, (0, 0, 255), 3)
+
+
+h = mean_shift([300, 100], good_matches, 0.001)
+cv2.circle(q_im, h, 2, (255, 0, 0), 3)
+cv2.imshow('show', q_im)
+cv2.waitKey(0)
+
+
+
+
