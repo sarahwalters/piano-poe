77054335d18e39bc69851420a0a3cea98e825568
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-12-06 22:08:11 -0500

    nameNote.py: Improved note center detection, working on splitting 8th Notes I now remove the stems of notes to improve center detection with means shift, this also allows me to determine the number of stems present, based on this, I am breaking images up into single notes, although currently the algorithm only works on notes where the stem is on the right side of the note.

--- OMR/nameNote.py
+++ OMR/nameNote.py
@@ -1,4 +1,5 @@
 import cv2
+import cv2.cv as cv
 import numpy as np 
 
 def mean_shift(hypothesis, keypoints, threshold):
@@ -55,6 +56,66 @@ def mean_shift(hypothesis, keypoints, threshold):
             diff = np.sqrt((last_guess[0] - x)**2 + (last_guess[1] - y)**2)
         return hypothesis
 
+def find_stems(image):
+    """
+    Takes in an image and isolates the number and positions of the stems, then 
+    breaks up the image as necessary and removes the stems 
+
+    inputs:
+        image -> a binary numpy array
+    returns:
+        stem_number -> the number of stems in the image
+        new_images -> a list of images with stems removed
+    """
+
+    #creating a list x values
+    stems = []
+    for x in range(len(image[0])):
+        column = image[0:len(image), x]
+        stem_length = len(column) - sum(column)/255
+        if stem_length >= 45:
+            stems.append(x)
+
+    #removing stems and counting their number
+    if len(stems) != 0:
+        num_stems = 1
+        current_stem = stems[0]
+        divisions = [stems[0]]
+        for row in stems:
+            #color stems white to remove
+            image[0:len(image), row] = 255
+            if row != stems[0]:
+                #count the number of stems
+                if abs(current_stem - row) > 5:
+                    current_stem = row
+                    num_stems = num_stems + 1
+                    #add the stem to a list to divide images
+                    divisions.append(current_stem)
+    
+    # cv2.imshow('test', image)
+    # cv2.waitKey(0)
+    if len(divisions) > 1:
+        images = []
+        start = 0
+        for x in divisions:
+            end = x + 5
+            segment = image[0:len(image[0]), start:x]
+            images.append(segment)
+            start = x
+            #TODO: add in protection for dealing with stems on the left side of the note (probably if black switch to next line as divider)
+            # cv2.imshow('test', segment)
+            # cv2.waitKey(0)
+
+    else:
+        num_stems = 0
+        images = len[image]
+
+
+    print len(images)
+    return image
+
+
+
 def isolate_center(image):
     """
     Takes in a binary numpy array and pulls out the coordinates of the black pixels
@@ -65,7 +126,7 @@ def isolate_center(image):
     returns:
         center -> the center of mass of the note
     """
-    #finds the points which are not black
+    #finds the points which are black
     points = []
     for row in range(len(image)):
       for col in range(len(image[row])):
@@ -73,8 +134,19 @@ def isolate_center(image):
             points.append((row,col))
 
     #finds the center of the image
-    center_row = len(image)/2
+    center_row = len(image)/2 
     center_col = len(image[0])/2
     center = (center_row, center_col)
-    center = mean_shift(center, points, 0.25)
+    center = mean_shift((100, 46), points, 0.25)
     return center
+
+if __name__ == '__main__':
+    im = cv2.imread('../images/lonely_quarters.png', 0)
+    image = find_stems(im)
+    # show_im = cv2.imread('../images/note3.png')
+    center = isolate_center(im)
+
+    # cv2.circle(image, (center[1], center[0]), 4, (255, 255, 255))
+    # cv2.imshow('test', image)
+    # cv2.waitKey(0)
+
